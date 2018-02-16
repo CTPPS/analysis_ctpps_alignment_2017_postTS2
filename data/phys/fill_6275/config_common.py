6b3455d5a21ab75a0c28db0e8736bf3e95cbbcb3
Jan Kašpar <jan.kaspar@cern.ch>
2018-02-16 17:59:38 +0100

    Tuned per-fill cut parameters.

---
+++ data/phys/fill_6275/config_common.py
@@ -0,0 +1,13 @@
+import sys 
+import os
+import FWCore.ParameterSet.Config as cms
+
+sys.path.append(os.path.relpath("../../../../../"))
+
+from config_base import config
+
+#config.sector_45.cut_h_c = -38.55
+#config.sector_45.cut_v_c = 1.63
+
+config.sector_56.cut_h_c = -39.34
+config.sector_56.cut_v_c = 1.39
---
+++ data/phys/fill_6346/config_common.py
@@ -0,0 +1,13 @@
+import sys 
+import os
+import FWCore.ParameterSet.Config as cms
+
+sys.path.append(os.path.relpath("../../../../../"))
+
+from config_base import config
+
+config.sector_45.cut_h_c = -38.57
+config.sector_45.cut_v_c = 1.63
+
+config.sector_56.cut_h_c = -39.31
+config.sector_56.cut_v_c = 1.46
