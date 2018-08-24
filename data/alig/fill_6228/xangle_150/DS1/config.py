28615f0380f44a993a2b091ea5e763e6f500982c
Jan Kašpar <jan.kaspar@cern.ch>
2018-08-24 15:15:25 +0200

    Parameters for method o adjusted.

--- data/alig/fill_6228/xangle_150/DS1/config.py
+++ data/alig/fill_6228/xangle_150/DS1/config.py
@@ -22,6 +22,15 @@ config.sector_56.cut_h_c = 0.08
 config.sector_56.cut_v_a = -1.07
 config.sector_56.cut_v_c = 0.18
 
+config.sector_45.nr_x_slice_min = 2
+config.sector_45.nr_x_slice_max = 18
+config.sector_45.fr_x_slice_min = 2
+config.sector_45.fr_x_slice_max = 19
+config.sector_56.nr_x_slice_min = 2
+config.sector_56.nr_x_slice_max = 15
+config.sector_56.fr_x_slice_min = 2
+config.sector_56.fr_x_slice_max = 15
+
 config.matching_1d.rp_L_2_F.x_min = 3
 config.matching_1d.rp_L_2_F.x_max = 15
 
@@ -33,3 +42,15 @@ config.matching_1d.rp_R_1_F.x_max = 14
 
 config.matching_1d.rp_R_2_F.x_min = 2
 config.matching_1d.rp_R_2_F.x_max = 14
+
+config.x_alignment_meth_o.rp_L_2_F.x_min = 5.
+config.x_alignment_meth_o.rp_L_2_F.x_max = 12.
+
+config.x_alignment_meth_o.rp_L_1_F.x_min = 5.
+config.x_alignment_meth_o.rp_L_1_F.x_max = 12.
+
+config.x_alignment_meth_o.rp_R_1_F.x_min = 4.
+config.x_alignment_meth_o.rp_R_1_F.x_max = 9.
+
+config.x_alignment_meth_o.rp_R_2_F.x_min = 4.
+config.x_alignment_meth_o.rp_R_2_F.x_max = 9.
